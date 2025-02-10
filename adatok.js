1d67a3d6668e8e978105cc620e2349822c9fa2f2
Bridgemaxxing <marthajozsef2@gmail.com>
2025-02-10 12:56:45 +0100

    elso

---
+++ adatok.js
@@ -0,0 +1,74 @@
+const nevekTomb=[
+    {
+        "megoldas":"Mátyás király",
+        "tipp1":"Lajos király",
+        "tipp2":"IV. Béla",
+        "tipp3":"II. András"
+    },
+    {
+        "megoldas":"Orbán Viktor",
+        "tipp1":"Gyurcsány Ferenc",
+        "tipp2":"Antall József",
+        "tipp3":"Kádár János"
+    },
+    {
+        "megoldas":"Winston Churchill",
+        "tipp1":"Franklin D. Roosevelt",
+        "tipp2":"Joseph Stalin",
+        "tipp3":"Charles de Gaulle"
+    },
+    {
+        "megoldas":"Zrínyi Miklós",
+        "tipp1":"Kinizsi Pál",
+        "tipp2":"Hunyadi János",
+        "tipp3":"Gábor Áron"
+    },
+    {
+        "megoldas":"Horthy Miklós",
+        "tipp1":"Teleki Pál",
+        "tipp2":"Gömbös Gyula",
+        "tipp3":"Bethlen István"
+    },
+    {
+        "megoldas":"Hunyadi János",
+        "tipp1":"II. András",
+        "tipp2":"Károly Róbert",
+        "tipp3":"I. Lajos"
+    },
+    {
+        "megoldas":"Mária Terézia",
+        "tipp1":"II. József",
+        "tipp2":"I. Lipót",
+        "tipp3":"Ferenc József"
+    },
+    {
+        "megoldas":"Rákóczi Ferenc",
+        "tipp1":"Koppány Attila",
+        "tipp2":"Rákóczi Gerzson",
+        "tipp3":"Bocskai János"
+    },
+    {
+        "megoldas":"Szulejmán",
+        "tipp1":"II. Mehmed",
+        "tipp2":"I. Szelim",
+        "tipp3":"II. Szelim"
+    },
+    {
+        "megoldas":"Nicolae Ceaușescu",
+        "tipp1":"Gheorghe Gheorghiu-Dej",
+        "tipp2":"Ion Iliescu",
+        "tipp3":"Emil Constantinescu"
+    },
+    {
+        "megoldas":"Szent István",
+        "tipp1":"Szent László",
+        "tipp2":"I. Géza",
+        "tipp3":"I. András"
+    },
+    {
+        "megoldas":"Petőfi Sándor",
+        "tipp1":"Arany János",
+        "tipp2":"Jókai Mór",
+        "tipp3":"Vörösmarty Mihály"
+    },
+];
